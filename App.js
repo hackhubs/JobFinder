17722ed5d60c63f46002dc0203ee7656689bf04a
hackhubs <abhav.thakur2018@vitstudent.ac.in>
2021-02-28 00:05:21 +0530

    dashboard

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
 import AuthNavigation from "./app/navigation/AuthNavigation";
-import FillJob from './app/screens/FillJob';
+import Home from "./app/screens/Employer/Home";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    // <FillJob />
-   <AuthNavigation />
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="Auth" component={AuthNavigation} />
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-  }
-  
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+}
